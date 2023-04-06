e7bf1681b72355d5fdd4d8d28cab59e7b0e58def
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-04-06 19:46:38 +0800

    created expense object

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,28 @@ import './App.css'
 import { ExpenseItem } from './components'
 
 function App() {
+  const expenses = [
+    {
+      id: 1,
+      title: 'Car Insurance',
+      amount: 342.54,
+      date: new Date(),
+    },
+
+    {
+      id: 2,
+      title: 'Car Insurance',
+      amount: 342.54,
+      date: new Date(),
+    },
+
+    {
+      id: 3,
+      title: 'Car Insurance',
+      amount: 342.54,
+      date: new Date(),
+    },
+  ]
 
   return (
     <div className="App">
