4ca07c50c48bb88d351a2d0b743dbed253342256
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-04-06 19:28:30 +0800

    updated styling for expense item component

--- src/components/expenseItem/ExpenseItem.jsx
+++ src/components/expenseItem/ExpenseItem.jsx
@@ -2,11 +2,17 @@ import './ExpenseItemStyle.css'
 import React from 'react'
 
 const ExpenseItem = () => {
+  const expenseDate = new Date(2021, 2, 28)
+  const expenseTitle = 'Car Insurance'
+  const expenseAmount = 294.67
+
   return (
     <div className='expense-item'>
-      <div>Date</div>
+      <div className="expense-item__title">
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__date'>{expenseDate.toISOString()}</div>
+      </div>
       <div className='expense-item__description'>
-        <h2>Title</h2>
         <div className='expense-item__price'>Amount</div>
       </div>
     </div>
