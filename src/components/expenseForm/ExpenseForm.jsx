5a97fab6e2f897c26c62cd12236955a2726440fb
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-04-08 14:53:20 +0800

    Update ExpenseForm.jsx

--- src/components/expenseForm/ExpenseForm.jsx
+++ src/components/expenseForm/ExpenseForm.jsx
@@ -28,46 +28,67 @@ const ExpenseForm = () => {
     })
   }
 
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+    const expenseData = {
+      title: userInput.enteredTitle,
+      amount: userInput.enteredAmount,
+      date: new Date(userInput.enteredDate)
+    }
+
+    setUserInput({
+      enteredTitle: '',
+      enteredAmount: '',
+      enteredDate: '',
+    })
+
+    console.log(expenseData)
+  }
+
 
   return (
     <>
-      <form action="">
+      <form action="" onSubmit={submitHandler}>
         <div className="new-expense__controls">
           <div className="new-expense__control">
-            <label htmlFor="">Title</label>
+            <label htmlFor="title">Title</label>
             <input 
               type="text" 
               name="" 
-              id="" 
+              id="title" 
+              value={userInput.enteredTitle}
               onChange={titleChangeHandler}
             />
           </div>
 
           <div className="new-expense__control">
-            <label htmlFor="">Amount</label>
+            <label htmlFor="amount">Amount</label>
             <input 
               type="number" 
               name="" 
-              id="" 
+              id="amount" 
               min="0.01" 
               step="0.01" 
+              value={userInput.enteredAmount}
               onChange={amountChangeHandler}
             />
           </div>
           <div className="new-expense__control">
-            <label htmlFor="">Date</label>
+            <label htmlFor="date">Date</label>
             <input 
               type="date" 
               name="" 
-              id="" 
+              id="date" 
               min="2019-01-01" 
               max="2025-12-31" 
+              value={userInput.enteredDate}
               onChange={dateChangeHandler}
             />
           </div>
         </div>
         <div className="new-expense__actions">
-          <button type='submit'>Add Expense</button>
+          <button type='submit' >Add Expense</button>
         </div>
       </form>
     </>
